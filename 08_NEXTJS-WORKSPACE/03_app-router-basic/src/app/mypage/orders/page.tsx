beeb5231e6fd86b673a94b274bb23f286d621fc0
jaey <jaeyyi98@gmail.com>
2025-11-26 16:25:36 +0900

    08

---
+++ 08_NEXTJS-WORKSPACE/03_app-router-basic/src/app/mypage/orders/page.tsx
@@ -0,0 +1,9 @@
+export default function OrderPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold ">주문페이지</h1>
+      <p>URL :/</p>
+      <p>파일경로 : app/car/orders/page.txs</p>
+    </div>
+  );
+}
---
+++ 08_NEXTJS-WORKSPACE/03_app-router-basic/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>th page does not exist</p>
+    </div>
+  );
+}
